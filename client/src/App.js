3abdd2cb828ea6d1460762f8665e587b7ae4f0c6
monguis <jccnnetwork@gmail.com>
2020-04-14 15:49:16 -0600

    created basic template for movieInfo page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios"
-import Home from "./pages/Home"
-axios.get('/').then((data) =>{
+import axios from "axios";
+import Home from "./pages/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieInfo from "./pages/MovieInfo";
+
+
+axios.get('/api/movies').then((data) =>{
   console.log(data);
 })
 
@@ -16,6 +20,7 @@ function App() {
         <Switch>
           <Route exact path={["/","/home"]}>
             <Home />
+            <MovieInfo/>
           </Route>
         </Switch>
     </Router>
---
+++ client/src/pages/MovieInfo/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+
+const MovieInfo = (props) => {
+
+    return (<>
+    <img src="https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SX300.jpg"></img>
+    <h1>Title</h1>
+    <h3>Genres, ReleaseDate, Duration, </h3>
+    <button>favorited?</button>
+    <br></br><br></br><br></br>
+<h3>PLot</h3>
+<p>loremljafjlkasdfl;khasd;fkhalfjdkhgvaslhdvaslhkfdvhsldgfhvlbalfhvlkasflkhilvkhslhdfbhsifvlhafkdjvblsfdjbkvlsnljfdv</p>
+<h3>Directed by:</h3>
+<p>loremljafjlkasdfl;khasd;fkhalfjdkhgvaslhdvaslhkfdvhsldgfhvlbalfhvlkasflkhilvkhslhdfbhsifvlhafkdjvblsfdjbkvlsnljfdv</p>
+<h3>production</h3>
+<p>loremljafjlkasdfl;khasd;fkhalfjdkhgvaslhdvaslhkfdvhsldgfhvlbalfhvlkasflkhilvkhslhdfbhsifvlhafkdjvblsfdjbkvlsnljfdv</p>
+<h3>Starring</h3>
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+</ul>
+    </>)
+}
+
+export default MovieInfo;
+
+// "tt8579674": {
+//     "Title": "1917",
+//     "Year": "2019",
+//     "Rated": "R",
+//     "Released": "10 Jan 2020",
+//     "Runtime": "119 min",
+//     "Genre": "Drama, War",
+//     "Director": "Sam Mendes",
+//     "Writer": "Sam Mendes, Krysty Wilson-Cairns",
+//     "Actors": "Dean-Charles Chapman, George MacKay, Daniel Mays, Colin Firth",
+//     "Plot": "April 6th, 1917. As a regiment assembles to wage war deep in enemy territory, two soldiers are assigned to race against time and deliver a message that will stop 1,600 men from walking straight into a deadly trap.",
+//     "Language": "English, French, German",
+//     "Country": "USA, UK, India, Spain, Canada",
+//     "Awards": "Won 3 Oscars. Another 103 wins & 152 nominations.",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SX300.jpg",
+//     "Ratings": [
+//       {
+//         "Source": "Internet Movie Database",
+//         "Value": "8.5/10"
+//       },
+//       {
+//         "Source": "Rotten Tomatoes",
+//         "Value": "89%"
+//       },
+//       {
+//         "Source": "Metacritic",
+//         "Value": "78/100"
+//       }
+//     ],
+//     "Metascore": "78",
+//     "imdbRating": "8.5",
+//     "imdbVotes": "164,576",
+//     "imdbID": "tt8579674",
+//     "Type": "movie",
+//     "DVD": "N/A",
+//     "BoxOffice": "N/A",
+//     "Production": "Universal Pictures",
+//     "Website": "N/A",
+//     "Response": "True"
+//   }
