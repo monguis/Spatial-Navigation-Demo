91eab2f48acbd301817855b839e3abdd1bed4709
monguis <jccnnetwork@gmail.com>
2020-05-15 10:50:49 -0600

    built deployment package

---
+++ client/build/precache-manifest.05ad41fa3e9ac1511af500a605454795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab6451ec2f71260c563d",
+    "url": "/static/js/main.8bddad5e.chunk.js"
+  },
+  {
+    "revision": "a857e6b81d9d1463ab38",
+    "url": "/static/js/2.ff7d5e77.chunk.js"
+  },
+  {
+    "revision": "ab6451ec2f71260c563d",
+    "url": "/static/css/main.82352261.chunk.css"
+  },
+  {
+    "revision": "a857e6b81d9d1463ab38",
+    "url": "/static/css/2.fcb621d9.chunk.css"
+  },
+  {
+    "revision": "1ddd06fa47332e639459fbad65e89abf",
+    "url": "/index.html"
+  }
+];
